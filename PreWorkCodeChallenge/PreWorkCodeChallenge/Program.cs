0d2fda5b8c58bd62d31fad260fd8f37e84e37a3d
unknown <areyes986@gmail.com>
2020-03-12 19:51:45 -0700

    able to have user pick 1-10 and chose number from array

--- PreWorkCodeChallenge/PreWorkCodeChallenge/Program.cs
+++ PreWorkCodeChallenge/PreWorkCodeChallenge/Program.cs
@@ -6,7 +6,54 @@ namespace PreWorkCodeChallenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            arrayMaxResult();
+            //leapYearCalender();
+
         }
+
+        private static void arrayMaxResult()
+        {
+
+            int[] numArray = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Guess a number from 1-10: ");
+                int input = int.Parse(Console.ReadLine());
+                while ( input > 10)
+                {
+                    Console.WriteLine("Numbers from 1-10 only please!");
+                    input = int.Parse(Console.ReadLine());
+                }
+                numArray[i] = input;
+            }
+
+            Console.WriteLine(string.Join(", ", numArray));
+            Console.Write("Pick a number from  list and find out your score: ");
+
+
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                int chosen = int.Parse(Console.ReadLine());
+
+                if (chosen == numArray[i])
+                {
+                    Console.WriteLine("You picked the same numnber");
+
+                }
+                else if (chosen != numArray[i])
+                {
+                    Console.WriteLine("Please pick a number from the list");
+                }
+            }
+            Console.ReadLine();
+        }
+
+        /*
+        private static void leapYearCalender()
+        {
+
+        }
+        */
     }
 }
